887b30803160850e849daeb7257f3c127c39c408
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2022-12-28 10:19:07 +0800

    PWA First test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'backgrounds/*', 'icons/*'],
+      manifest,
+    }),
+  ],
+});
