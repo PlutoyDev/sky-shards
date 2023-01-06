9b53b83bc379bea22b11875feaa29ea2a2af4d38
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-01-06 23:16:45 +0800

    Added back footer and animates the credits

--- src/sections/App/Footer.tsx
+++ src/sections/App/Footer.tsx
@@ -3,26 +3,28 @@ import credits from '../../data/credits';
 export default function Footer() {
   return (
     <div id='footer' className='glass'>
-      {/* <div>
-        <span>Not affiliated with thatgamecompany</span>
+      <div id='footer-disclaimer'>
+        <span>Not affiliated with thatgamecompany </span>
         <span>(It might not reflect what is in-game)</span>
       </div>
-      <div>
+      <div id='footer-author'>
         <span>Created by Plutoy</span>
         <div className='links'>
           <a href='https://github.com/PlutoyDev/sky-shards'>View source code</a>
-          Or
+          <span> Or </span>
           <a href='https://github.com/PlutoyDev/sky-shards/issues/new'>Report an issue</a>
         </div>
       </div>
-      <div>
+      <div id='footer-credits'>
         <span>Thank you to those who helped to discover the patterns shard eruption:</span>
-        <div className='credits'>
-          {credits.map(u => (
-            <span key={u}>{u}</span>
-          ))}
+        <div className='scrolling'>
+          <div id='credits'>
+            {credits.map(u => (
+              <span key={u}>{u}</span>
+            ))}
+          </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
