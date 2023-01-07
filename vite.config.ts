6c4c4f402621f2b6b52bd942fe3a4bafc4d63cb1
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-01-07 22:50:12 +0800

    Added runtime caching

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,52 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'robots.txt', 'backgrounds/*', 'icons/*'],
       manifest,
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-stylesheets',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-webfonts',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          //Not just images though. This is a catch all for all netlify cdn domains
+          process.env.NETLIFY_IMAGES_CDN_DOMAIN && {
+            urlPattern: process.env.NETLIFY_IMAGES_CDN_DOMAIN + '/.*/',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'netlify-cdn',
+              backgroundSync: {
+                name: 'netlify-cdn-queue',
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
     }),
   ],
 });
