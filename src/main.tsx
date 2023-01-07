34d7131809a7c842334320925917b4482efb9ef0
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-01-07 23:43:38 +0800

    Remove useless fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import router from './Router';
 import './index.css';
 
