1fd64e9c8ce1fe760e04b0e2ec581cc00898e87d
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-01-07 08:45:08 +0800

    Footer Icons

--- src/sections/App/Footer.tsx
+++ src/sections/App/Footer.tsx
@@ -1,3 +1,4 @@
+import { BsGithub, BsTwitter, BsReddit, BsDiscord, BsFillEnvelopeFill } from 'react-icons/bs';
 import credits from '../../data/credits';
 
 export default function Footer() {
@@ -18,11 +19,54 @@ export default function Footer() {
         {/* </div> */}
       </div>
       <div id='footer-author'>
-        <span>Created by Plutoy</span>
-        <div className='links'>
-          <a href='https://github.com/PlutoyDev/sky-shards'>View source code</a>
-          <span> Or </span>
-          <a href='https://github.com/PlutoyDev/sky-shards/issues/new'>Report an issue</a>
+        <div>
+          <div id='author'>Created by Plutoy</div>
+          <div id='author-link'>
+            <a className='circular-icon' href='https://github.com/PlutoyDev' aria-label="Go to Developer's GitHub">
+              <BsGithub />
+            </a>
+            <a className='circular-icon' href='https://twitter.com/PlutoySky' aria-label="Go to Developer's Twitter">
+              <BsTwitter />
+            </a>
+            <a
+              className='circular-icon'
+              href='https://www.reddit.com/u/PlutoySky'
+              aria-label="Go to Developer's Reddit"
+            >
+              <BsReddit />
+            </a>
+            <a
+              className='circular-icon'
+              href='mailto:99151858+PlutoyDev@users.noreply.github.com'
+              aria-label='Email the developer'
+            >
+              <BsFillEnvelopeFill />
+            </a>
+          </div>
+        </div>
+        <div>
+          <div id='version'>
+            <div>
+              {import.meta.env.DEV ? 'Dev' : 'Prod'} Version:{' '}
+              {(import.meta.env.VITE_VERSION_MINOR && `V${import.meta.env.VITE_VERSION_MINOR}`) ??
+                (import.meta.env.VITE_NETLIFY_GIT_BRANCH && `Branch ${import.meta.env.VITE_NETLIFY_GIT_BRANCH}`) ??
+                (import.meta.env.VITE_NETLIFY_GIT_COMIT_REF &&
+                  `Commit ${import.meta.env.VITE_NETLIFY_GIT_COMIT_REF}`) ??
+                'Local'}
+            </div>
+          </div>
+          <div id='project-link'>
+            <a className='circular-icon' href='https://github.com/PlutoyDev/sky-shards' aria-label='Source Code'>
+              <BsGithub />
+            </a>
+            <a
+              className='circular-icon'
+              href='https://discord.com/channels/736912435654688868/1041548386333102090'
+              aria-label='Discord'
+            >
+              <BsDiscord />
+            </a>
+          </div>
         </div>
       </div>
     </div>
