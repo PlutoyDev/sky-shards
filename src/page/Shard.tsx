2148c3a4374695039f0ec8f9adc2b04e8965e3c0
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-02-08 07:07:55 +0800

    Navigation & animation to calendar

--- src/page/Shard.tsx
+++ src/page/Shard.tsx
@@ -4,6 +4,7 @@ import { createUseGesture, dragAction, pinchAction } from '@use-gesture/react';
 import { animate, motion, useMotionValue, useTransform } from 'framer-motion';
 import { DateTime } from 'luxon';
 import { useNow } from '../context/Now';
+import { ShardCalendar } from '../sections/Shard/Calendar';
 import { ShardDataInfographic, ShardMapInfographic } from '../sections/Shard/Infographic';
 import ShardSummary from '../sections/Shard/Summary';
 import ShardTimeline from '../sections/Shard/Timeline';
@@ -23,10 +24,12 @@ const relDateMap = {
 
 interface ShardLoaderData {
   isShardPage: true;
-  date: DateTime;
+  isCalendar?: boolean;
+  date?: DateTime;
 }
 
 const dragThreshold = 150;
+const pinchThreshold = 0.3;
 const useGesture = createUseGesture([dragAction, pinchAction]);
 const roundToRefDate = (date: DateTime, ref: DateTime) =>
   date.hasSame(ref, 'day') ? ref : date < ref ? date.endOf('day') : date.startOf('day');
@@ -34,7 +37,18 @@ const roundToRefDate = (date: DateTime, ref: DateTime) =>
 export const ShardPageLoader: LoaderFunction = ({ params }): Response | ShardLoaderData => {
   const [route, ...args] = params['*']?.split('/') ?? [];
 
-  if (route === 'date') {
+  if (route === 'calendar') {
+    const today = DateTime.local({ zone: 'America/Los_Angeles' });
+    const [yearStr, monthStr] = args;
+    const year = yearStr ? parseInt(yearStr.length === 2 ? `20${yearStr}` : yearStr, 10) : today.year;
+    const month = yearStr ? (monthStr ? parseInt(monthStr, 10) : 1) : today.month;
+
+    return {
+      isShardPage: true,
+      isCalendar: true,
+      date: DateTime.local(year, month, 1, { zone: 'America/Los_Angeles' }),
+    };
+  } else if (route === 'date') {
     const [yearStr, monthStr, dayStr] = args;
     const year = parseInt(yearStr.length === 2 ? `20${yearStr}` : yearStr, 10);
     const month = monthStr ? parseInt(monthStr, 10) : 1;
@@ -79,17 +93,25 @@ export const ShardPageLoader: LoaderFunction = ({ params }): Response | ShardLoa
   return redirect('/');
 };
 
+const pendableState = {
+  plus: 'plus',
+  minus: 'minus',
+  calendar: 'calendar',
+  date: 'date',
+};
+
 export default function Shard() {
   const now = useNow().application;
   const loaderData = (useLoaderData() ?? {}) as ShardLoaderData;
   const date = roundToRefDate(loaderData.date ?? now, now);
+  const isCalendar = loaderData.isCalendar;
 
   const navigate = useNavigate();
   const [isNavigatable, setIsNavigatable] = useState(true);
 
   const activeContentRef = useRef<HTMLDivElement>(null);
-  const pendingPreviousContentRef = useRef<HTMLDivElement>(null);
-  const pendingNextContentRef = useRef<HTMLDivElement>(null);
+  const pendingRef = useRef<HTMLDivElement>(null);
+  const [pendingState, setPendingState] = useState<typeof pendableState[keyof typeof pendableState] | null>(null);
   const [hintIdx, setHintIdx] = useState(0);
   const draggedX = useMotionValue(0);
 
@@ -114,34 +136,63 @@ export default function Shard() {
   const previousContentX = useTransform(contentX, x => x - window?.innerWidth);
   const nextContentX = useTransform(contentX, x => x + window?.innerWidth);
 
-  const navigateToPreviousDay = useCallback(() => {
+  const pinchDistance = useMotionValue(1);
+  const contentScale = useTransform(
+    pinchDistance,
+    [1 / pinchThreshold, 1, pinchThreshold],
+    [1 / (pinchThreshold * 1.2), 1, pinchThreshold * 1.2],
+  );
+  const contentOpacity = useTransform(
+    pinchDistance,
+    [1 / pinchThreshold, 1, pinchThreshold],
+    [pinchThreshold * 0.7, 1, pinchThreshold * 0.7],
+  );
+
+  const calendarScale = useTransform(contentScale, x => 1 + x);
+  const calendarOpacity = useTransform(contentOpacity, x => 1 - x);
+
+  const dateScale = useTransform(contentScale, x => x * 0.3);
+  const dateOpacity = useTransform(contentOpacity, x => x * 0.3);
+
+  const navigateLeftRight = useCallback(
+    (sign: -1 | 1) => {
+      setPendingState(sign === 1 ? pendableState.plus : pendableState.minus);
+      setIsNavigatable(false);
+
+      animate(contentX, -window.innerWidth * sign, {
+        type: 'spring',
+        duration: 0.5,
+        onComplete: () => {
+          navigate(
+            `/${isCalendar ? 'calendar' : 'date'}/${date
+              .plus({ [isCalendar ? 'months' : 'days']: sign })
+              .toFormat(`yyyy/MM/dd`)}`,
+          );
+          draggedX.jump(0);
+          setHintIdx(0);
+          setPendingState(null);
+          setIsNavigatable(true);
+        },
+      });
+    },
+    [date.day, date.month, date.year, draggedX, contentX, isCalendar],
+  );
+
+  const navigateToCalendar = useCallback(() => {
+    setPendingState(pendableState.calendar);
     setIsNavigatable(false);
-    animate(contentX, window.innerWidth, {
+    animate(contentScale, 0, { type: 'spring', duration: 0.5 });
+    animate(contentOpacity, 0, {
       type: 'spring',
-      duration: 0.5,
+      duration: 0.51,
       onComplete: () => {
-        navigate(`/date/${date.minus({ days: 1 }).toFormat(`yyyy/MM/dd`)}`);
-        draggedX.jump(0);
-        setHintIdx(0);
+        navigate(`/calendar/${date.toFormat(`yyyy/MM/dd`)}`);
+        pinchDistance.jump(1);
+        setPendingState(null);
         setIsNavigatable(true);
       },
     });
-  }, [date.day, date.month, date.year, draggedX, contentX]);
-
-  const navigateToNextDay = useCallback(() => {
-    setIsNavigatable(false);
-
-    animate(contentX, -window.innerWidth, {
-      type: 'spring',
-      duration: 0.5,
-      onComplete: () => {
-        navigate(`/date/${date.plus({ days: 1 }).toFormat(`yyyy/MM/dd`)}`);
-        draggedX.jump(0);
-        setHintIdx(0);
-        setIsNavigatable(true);
-      },
-    });
-  }, [date.day, date.month, date.year, draggedX, contentX]);
+  }, [date.day, date.month, date.year, contentScale, contentOpacity]);
 
   const bind = useGesture(
     {
@@ -151,20 +202,38 @@ export default function Shard() {
           setHintIdx(2 * Math.sign(mx));
         } else if (Math.abs(mx) > 20) {
           setHintIdx(1 * Math.sign(mx));
+          setPendingState(pendableState[mx < 0 ? 'plus' : 'minus']);
         } else {
           setHintIdx(0);
+          setPendingState(null);
         }
       },
       onDragEnd: ({ movement: [mx] }) => {
         if (Math.abs(mx) > dragThreshold) {
           if (mx > 0) {
-            navigateToPreviousDay();
+            navigateLeftRight(-1);
           } else {
-            navigateToNextDay();
+            navigateLeftRight(1);
           }
         } else {
           animate(draggedX, 0, { type: 'spring', stiffness: 500 });
           setHintIdx(0);
+        }
+      },
+      onPinch: ({ movement: [d] }) => {
+        if (!isCalendar && d < 1) {
+          pinchDistance.set(d);
+          setPendingState(pendableState.calendar);
+        }
+      },
+      onPinchEnd: ({ movement: [d] }) => {
+        if (!isCalendar) {
+          if (d < pinchThreshold) {
+            navigateToCalendar();
+          } else {
+            animate(pinchDistance, 1, { type: 'spring', stiffness: 500 });
+            setPendingState(null);
+          }
         }
       },
     },
@@ -174,42 +243,59 @@ export default function Shard() {
   );
 
   return (
-    <main className='Page ShardPage'>
+    <main className='Page ShardPage' {...bind()}>
       <ShardPageContent
         ref={activeContentRef}
         date={date}
-        divProps={{ ...bind() }}
-        style={{
-          x: contentX,
-        }}
-      />
-      <ShardPageContent
-        ref={pendingPreviousContentRef}
-        date={roundToRefDate(date.minus({ days: 1 }), now)}
-        style={{
-          x: previousContentX,
-        }}
+        isCalendar={isCalendar}
+        style={{ x: contentX, scale: contentScale, opacity: contentOpacity }}
       />
 
-      <ShardPageContent
-        ref={pendingNextContentRef}
-        date={roundToRefDate(date.plus({ days: 1 }), now)}
-        style={{
-          x: nextContentX,
-        }}
-      />
+      {pendingState &&
+        {
+          [pendableState.plus]: (
+            <ShardPageContent
+              ref={pendingRef}
+              date={roundToRefDate(date.plus({ [isCalendar ? 'months' : 'days']: 1 }), now)}
+              isCalendar={isCalendar}
+              style={{ x: nextContentX }}
+            />
+          ),
+          [pendableState.minus]: (
+            <ShardPageContent
+              ref={pendingRef}
+              date={roundToRefDate(date.minus({ [isCalendar ? 'months' : 'days']: 1 }), now)}
+              isCalendar={isCalendar}
+              style={{ x: previousContentX }}
+            />
+          ),
+          [pendableState.calendar]: (
+            <ShardPageContent
+              ref={pendingRef}
+              date={date}
+              isCalendar={true}
+              style={{ scale: calendarScale, opacity: calendarOpacity }}
+            />
+          ),
+          [pendableState.date]: (
+            <ShardPageContent
+              ref={pendingRef}
+              date={date}
+              isCalendar={false}
+              style={{ scale: dateScale, opacity: dateOpacity }}
+            />
+          ),
+        }[pendingState]}
+
       <NavHint
         position='left'
         disabled={Math.sign(hintIdx) === -1 || !isNavigatable}
         hideArrow={Math.abs(hintIdx) === 2}
         hint={
-          [
-            'Swipe right or Click here for previous day shard',
-            'Swipe further for previous day shard',
-            'Release for previous day shard',
-          ][Math.abs(hintIdx)]
+          ['Swipe right or Click here', 'Swipe further', 'Release'][Math.abs(hintIdx)] +
+          ` for previous ${isCalendar ? 'month' : 'day'} shard`
         }
-        onClick={() => navigateToPreviousDay()}
+        onClick={() => navigateLeftRight(-1)}
         style={{
           x: hintLeftX,
         }}
@@ -219,13 +305,10 @@ export default function Shard() {
         disabled={Math.sign(hintIdx) === 1 || !isNavigatable}
         hideArrow={Math.abs(hintIdx) === 2}
         hint={
-          [
-            'Swipe left or Click here for next day shard',
-            'Swipe further for next day shard',
-            'Release for next day shard',
-          ][Math.abs(hintIdx)]
+          ['Swipe left or Click here', 'Swipe further', 'Release'][Math.abs(hintIdx)] +
+          ` for next ${isCalendar ? 'month' : 'day'} shard`
         }
-        onClick={() => navigateToNextDay()}
+        onClick={() => navigateLeftRight(1)}
         style={{
           x: hintRightX,
         }}
@@ -236,12 +319,21 @@ export default function Shard() {
 
 interface ShardPageContentProps {
   date: DateTime;
+  isCalendar?: boolean;
   divProps?: HTMLAttributes<HTMLDivElement>;
 }
 
 const ShardPageContent = motion(
-  forwardRef<HTMLDivElement, ShardPageContentProps>(function ShardPageContent({ date, divProps }, ref) {
-    const info = useMemo(() => getShardInfo(date), [date]);
+  forwardRef<HTMLDivElement, ShardPageContentProps>(function ShardPageContent({ date, isCalendar, divProps }, ref) {
+    if (isCalendar) {
+      return (
+        <div id='shardContent' ref={ref} {...divProps}>
+          <ShardCalendar year={date.year} month={date.month} />
+        </div>
+      );
+    }
+
+    const info = getShardInfo(date);
 
     return (
       <div id='shardContent' ref={ref} {...divProps}>
