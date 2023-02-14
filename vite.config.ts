d9f18aac33eb9e7603bf7b1fbe45e6ff961b337a
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-02-14 19:42:13 +0800

    update included assets to include sitemap.txt and fix workbox config

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'backgrounds/*', 'icons/*'],
+      includeAssets: ['favicon.ico', 'robots.txt', 'sitemap.txt', 'backgrounds/*', 'icons/*'],
       manifest,
       workbox,
     }),
