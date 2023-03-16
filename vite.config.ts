97b0c45252a3f914bef20582a1cf6a823826698b
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-03-16 10:36:13 +0800

    Remove vite plugins.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,4 @@
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import manifest from './manifest';
-import workbox from './workbox';
-
-console.log('Version', process.env.VITE_VERSION_MINOR);
-console.log('Branch', process.env.BRANCH);
-console.log('Commit Ref', process.env.COMMIT_REF);
-console.log('Netlify CDN', process.env.NETLIFY_IMAGES_CDN_DOMAIN);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,13 +6,4 @@ export default defineConfig({
     __NETLIFY_GIT_BRANCH__: `'${process.env.BRANCH}'`,
     __NETLIFY_GIT_COMMIT_REF__: `'${process.env.COMMIT_REF}'`,
   },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'fonts/*', 'backgrounds/*', 'icons/*', 'infographics/**/*.webp', 'emojis/*.webp'],
-      manifest,
-      workbox,
-    }),
-  ],
 });
