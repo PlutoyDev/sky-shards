ec502b2c59dfdf8f14275c14f289a12beda058a2
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2023-06-03 09:42:41 +0800

    Move settings related into Setting context

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,26 @@
-import { useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
 import { HeaderFxProvider } from './context/HeaderFx';
 import { ModalProvider } from './context/ModalContext';
 import { NowProvider } from './context/Now';
 import { SettingsProvider } from './context/Settings';
-import useLocalStorageState from './hooks/useLocalStorageState';
 import Footer from './sections/App/Footer';
 import Header from './sections/App/Header';
 import ShardCarousel from './sections/Shard/Carousel';
 
 function App() {
-  const [twelveHourModeSetting, setTwelveHourModeSetting] = useLocalStorageState('twelveHourMode', 'system');
-  const [lightMode, setLightMode] = useLocalStorageState<'false' | 'true' | 'system'>('lightMode', 'system');
-  const compactMode = useMediaQuery({ maxWidth: '300px' });
-
-  const twelveHourMode =
-    twelveHourModeSetting === 'system'
-      ? Intl.DateTimeFormat().resolvedOptions().hour12 ?? false
-      : twelveHourModeSetting === 'true';
-
-  const boolLightMode =
-    lightMode === 'true' || (lightMode === 'system' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
-
-  useEffect(() => {
-    if (lightMode === 'system') {
-      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      if (prefersDarkMode) {
-        document.body.classList.remove('light');
-      } else {
-        document.body.classList.add('light');
-      }
-    } else if (lightMode === 'true') {
-      document.body.classList.add('light');
-    } else if (lightMode === 'false') {
-      document.body.classList.remove('light');
-    }
-  }, [lightMode]);
-
   return (
-    <ModalProvider>
-      <HeaderFxProvider>
-        <SettingsProvider
-          value={{ isTwelveHourMode: twelveHourMode, isLightMode: boolLightMode, isCompactMode: compactMode }}
-        >
-          <NowProvider>
+    <HeaderFxProvider>
+      <SettingsProvider>
+        <NowProvider>
+          <ModalProvider>
             <div className='App'>
-              <Header
-                setTwelveHourModeSetting={setTwelveHourModeSetting}
-                setLightMode={setLightMode}
-                twelveHourModeSetting={twelveHourModeSetting}
-                lightMode={lightMode}
-              />
+              <Header />
               <ShardCarousel />
               <Footer />
             </div>
-          </NowProvider>
-        </SettingsProvider>
-      </HeaderFxProvider>
-    </ModalProvider>
+          </ModalProvider>
+        </NowProvider>
+      </SettingsProvider>
+    </HeaderFxProvider>
   );
 }
 
--- src/context/Settings.tsx
+++ src/context/Settings.tsx
@@ -1,18 +1,80 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import useLocalStorageState from '../hooks/useLocalStorageState';
 
 export interface Settings {
   isTwelveHourMode: boolean;
   isLightMode: boolean;
   isCompactMode: boolean;
+
+  setTwelveHourModeSetting: (value: string) => void;
+  setLightMode: (value: string) => void;
+  twelveHourModeSetting: string;
+  lightMode: string;
 }
 
 export const SettingsContext = createContext<Settings>({
   isTwelveHourMode: false,
   isLightMode: false,
   isCompactMode: false,
+
+  setTwelveHourModeSetting: () => console.log('setTwelveHourModeSetting not yet initialized'),
+  setLightMode: () => console.log('setLightMode not yet initialized'),
+
+  twelveHourModeSetting: 'system',
+  lightMode: 'system',
 });
 
 export const useSettings = () => useContext(SettingsContext);
 
-export const SettingsProvider = SettingsContext.Provider;
 export const SettingsConsumer = SettingsContext.Consumer;
+
+interface SettingsProviderProps {
+  children: React.ReactNode;
+}
+
+export function SettingsProvider({ children }: SettingsProviderProps) {
+  const [twelveHourModeSetting, setTwelveHourModeSetting] = useLocalStorageState('twelveHourMode', 'system');
+  const [lightMode, setLightMode] = useLocalStorageState<'false' | 'true' | 'system'>('lightMode', 'system');
+  const compactMode = useMediaQuery({ maxWidth: '300px' });
+
+  const twelveHourMode =
+    twelveHourModeSetting === 'system'
+      ? Intl.DateTimeFormat().resolvedOptions().hour12 ?? false
+      : twelveHourModeSetting === 'true';
+
+  const boolLightMode =
+    lightMode === 'true' || (lightMode === 'system' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+  useEffect(() => {
+    if (lightMode === 'system') {
+      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      if (prefersDarkMode) {
+        document.body.classList.remove('light');
+      } else {
+        document.body.classList.add('light');
+      }
+    } else if (lightMode === 'true') {
+      document.body.classList.add('light');
+    } else if (lightMode === 'false') {
+      document.body.classList.remove('light');
+    }
+  }, [lightMode]);
+
+  return (
+    <SettingsContext.Provider
+      value={{
+        isTwelveHourMode: twelveHourMode,
+        isLightMode: boolLightMode,
+        isCompactMode: compactMode,
+
+        setTwelveHourModeSetting,
+        setLightMode,
+        twelveHourModeSetting,
+        lightMode,
+      }}
+    >
+      {children}
+    </SettingsContext.Provider>
+  );
+}
