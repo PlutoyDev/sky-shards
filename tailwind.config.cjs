14dee511faf0405f2ff7ea4dcfe704239c6895df
PlutoyDev <plutoy.dev@outlook.com>
2023-08-13 01:45:20 +0800

    convert settings modal to daisyui theming

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,10 @@
+const colors = require('tailwindcss/colors')
+
+const themeDef = {
+  "base-100": colors.neutral[800],
+  "--btn-text-case": "full-width",
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -11,11 +18,17 @@ module.exports = {
       {
         light: {
           ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          ...themeDef,
+          "primary": colors.sky[300],
+          "primary-content": colors.black,
         },
         dark: {
           ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          ...themeDef,
+          "primary": colors.violet[600],
+          "primary-content": colors.white,
         }
-      }
+      },
     ],
   }
 };
