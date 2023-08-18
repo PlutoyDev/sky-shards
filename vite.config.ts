5d2a31641b88f7a6eb71a576c10930e739f2ae20
PlutoyDev <plutoy.dev@outlook.com>
2023-08-18 22:33:21 +0800

    Use version from packaage.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import manifest from './manifest';
+import packageJson from './package.json';
 import workbox from './workbox';
 
-console.log('Version', process.env.VITE_VERSION_MINOR);
-console.log('Branch', process.env.CF_PAGES_BRANCH);
-console.log('Commit Ref', process.env.CF_PAGES_COMMIT_SHA);
-
+process.env.VITE_VERSION = packageJson.version;
+process.env.VITE_VERSION_MINOR = packageJson.version.split('.').slice(0, 2).join('.');
 process.env.VITE_GIT_BRANCH = process.env.CF_PAGES_BRANCH;
 process.env.VITE_GIT_COMMIT = process.env.CF_PAGES_COMMIT_SHA;
+
+console.log('Version', process.env.VITE_VERSION);
+console.log('Branch', process.env.VITE_GIT_BRANCH);
+console.log('Commit Ref', process.env.VITE_GIT_COMMIT);
 
 // https://vitejs.dev/config/
 export default defineConfig({
