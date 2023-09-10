e836703908e540bcaba19346c424a45996c439eb
PlutoyDev <plutoy.dev@outlook.com>
2023-09-10 16:10:50 +0800

    fix clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -29,7 +29,7 @@ export function Clock({
         hideSeconds ? (Math.abs(duration.as('minutes')) > 90 ? `hh'h' mm'm'` : `mm'm' ss's'`) : `hh'h' mm'm' ss's'`,
       )
     : time?.setZone(convertTo === 'local' ? 'local' : 'America/Los_Angeles')?.toLocaleString({
-        hourCycle: twelveHourModeSetting === 'system' ? undefined : twelveHourModeSetting === 'true' ? 'h23' : 'h12',
+        hourCycle: twelveHourModeSetting === 'system' ? undefined : twelveHourModeSetting === 'true' ? 'h12' : 'h23',
         hour: '2-digit',
         minute: '2-digit',
         second: hideSeconds ? undefined : '2-digit',
