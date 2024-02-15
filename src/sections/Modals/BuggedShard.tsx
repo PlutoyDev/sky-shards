af018398559fd389d009b2394e2f8d6ab358639b
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2024-02-15 12:48:28 +0000

    fix grammer

--- src/sections/Modals/BuggedShard.tsx
+++ src/sections/Modals/BuggedShard.tsx
@@ -7,8 +7,8 @@ export function ShardIsBuggedModal({ hideModal }: ModalProps) {
   return (
     <div className='my-2 flex max-h-full w-full flex-col flex-nowrap items-start justify-center gap-y-2'>
       <p className='text-lg font-semibold'>Hey there skykids, </p>
-      <p>You are seeing this as pop-up because shard is currently bugged in-game or schedule of shard had changed.</p>
-      <p>There the countdown/clock prediction here might not reflect what is in game</p>
+      <p>You are seeing this pop-up because shard is currently bugged in-game or the schedule of shard had changed.</p>
+      <p>The countdown/prediction here might not reflect what is in game</p>
       <p>
         You can still use this app by click on the proceed button bellow (daily) until the prediction is correct again
       </p>
