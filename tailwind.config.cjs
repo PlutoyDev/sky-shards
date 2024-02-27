895b3cd0883a79bb140c4c4e517ca3a698b4f831
PlutoyDev <plutoy.dev@outlook.com>
2024-02-27 09:18:57 +0800

    fix daisyui breaking change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,41 +1,39 @@
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 const themeDef = {
-  "base-100": colors.neutral[800],
-  "--btn-text-case": "full-width",
-}
+  'base-100': colors.neutral[800],
+  '--btn-text-case': 'full-width',
+};
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('daisyui'),
-  ],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   theme: {
     screens: {
-      'xs': '375px',
+      xs: '375px',
       ...defaultTheme.screens,
     },
   },
   daisyui: {
-    darkTheme: "night",
+    darkTheme: 'night',
     themes: [
       {
         light: {
-          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          ...require('daisyui/src/theming/themes')['light'],
           ...themeDef,
-          "primary": colors.sky[300],
-          "primary-content": colors.black,
+          'primary': colors.sky[300],
+          'primary-content': colors.black,
         },
         dark: {
-          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          ...require('daisyui/src/theming/themes')['dark'],
           ...themeDef,
-          "primary": colors.violet[600],
-          "primary-content": colors.white,
-        }
+          'primary': colors.violet[600],
+          'primary-content': colors.white,
+        },
       },
     ],
-  }
+  },
 };
