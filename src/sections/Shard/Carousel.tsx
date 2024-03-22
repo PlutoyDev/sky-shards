c2f00bb0ae2055eec0046656a100519d4b65dd21
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-03-23 07:55:44 +0800

    reattempting carousel without framer motion

--- src/sections/Shard/Carousel.tsx
+++ src/sections/Shard/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback, useRef, cloneElement } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import { createUseGesture, dragAction } from '@use-gesture/react';
@@ -11,47 +11,35 @@ const useDrag = createUseGesture([dragAction]);
 
 const appZone = 'America/Los_Angeles';
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
 const roundToRefDate = (date: DateTime, ref: DateTime) =>
   date.hasSame(ref, 'day') ? ref : date < ref ? date.endOf('day') : date.startOf('day');
 
 export default function ShardCarousel() {
   const { t } = useTranslation(['shardCarousel']);
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLElement | null>(null);
+  const exitingRef = useRef<HTMLDivElement>(null);
+  const primaryElementRef = useRef<JSX.Element | null>(null);
+  const exitingElementRef = useRef<JSX.Element | null>(null);
 
   const [date, setDate] = useState(() =>
     roundToRefDate(parseUrl().date, DateTime.local().setZone(appZone).startOf('day')),
   );
 
+  const previousDate = useRef<DateTime>(date);
+
   const navigateDate = useCallback(
     async (d: DateTime | number, reUrl = true) => {
       const today = DateTime.local().setZone('America/Los_Angeles').startOf('day');
-      const dir = typeof d === 'number' ? d : d > date ? 1 : -1;
       const toDate = typeof d === 'number' ? date.plus({ days: d }) : d;
 
       if (date.hasSame(toDate, 'day')) return;
 
-      if (ref.current) {
-        ref.current.style.transitionProperty = 'transform, opacity';
-        ref.current.style.transform = `translateX(${-dir * 100}%)`;
-        ref.current.style.opacity = '0';
-        await sleep(75); // 1/2 transition duration
-      }
       if (toDate.hasSame(today, 'day')) {
         if (reUrl) replaceUrl({ date: today });
         setDate(today);
       } else {
         if (reUrl) replaceUrl({ date: toDate });
         setDate(roundToRefDate(toDate, today));
-      }
-      if (ref.current) {
-        ref.current.style.transitionProperty = 'none';
-        ref.current.style.transform = `translateX(${dir * 100}%)`;
-        await sleep(75); // 1/2 transition duration
-        ref.current.style.transitionProperty = 'transform, opacity';
-        ref.current.style.transform = 'translateX(0)';
-        ref.current.style.opacity = '1';
       }
     },
     [date],
@@ -90,15 +78,77 @@ export default function ShardCarousel() {
       if (ref.current) {
         ref.current.style.transitionDuration = '150ms';
         const swipePower = x * vx;
-        if (Math.abs(swipePower) > 400) navigateDate(Math.sign(x) * -1);
+        if (Math.abs(swipePower) > 200) navigateDate(Math.sign(x) * -1);
         else ref.current.style.transform = 'translateX(0)';
       }
     },
   });
 
+  useEffect(() => {
+    const dateChanged = !previousDate.current.hasSame(date, 'day');
+    const dir = previousDate.current < date ? 'rtl' : 'ltr';
+    // Set the exiting element
+    if (dateChanged && primaryElementRef.current) {
+      exitingElementRef.current = cloneElement(primaryElementRef.current, {
+        ref: exitingRef,
+        style: { transition: 'transform 150ms ease-in-out', fontSize: `${fontSize}em` },
+      });
+    }
+
+    // Set the primary element
+    if (primaryElementRef.current == null || dateChanged) {
+      primaryElementRef.current = (
+        <ShardMain
+          ref={ref}
+          key={date.toISODate()}
+          date={date}
+          {...bind()}
+          style={{
+            fontSize: `${fontSize}em`,
+            transform: !primaryElementRef.current
+              ? undefined
+              : dir === 'rtl'
+                ? 'translateX(100%)'
+                : 'translateX(-100%)',
+            transition: 'transform 150ms ease-in-out',
+          }}
+        />
+      );
+    }
+
+    // If the date is the same, don't animate
+    if (!dateChanged) return;
+
+    // If the exiting element is not null, animate
+    if (exitingElementRef.current) {
+      setTimeout(() => {
+        if (exitingRef.current) {
+          exitingRef.current.style.transform = dir === 'rtl' ? 'translateX(-100%)' : 'translateX(100%)';
+          exitingRef.current.addEventListener('transitionend', () => {
+            exitingElementRef.current = null;
+            if (ref.current) {
+              ref.current.style.transform = 'translateX(0)';
+            }
+          });
+        }
+      }, 0); // Wait for the next frame
+    }
+
+    // Set the previous date
+    previousDate.current = date;
+  }, [date, bind]);
+
+  useEffect(() => {
+    // Set font size
+    if (ref.current) {
+      ref.current.style.fontSize = `${fontSize}em`;
+    }
+  }, [fontSize]);
+
   return (
     <div className='grid h-full max-h-full w-full select-none grid-cols-[2rem_auto_2rem] grid-rows-[auto] items-center justify-items-center gap-1 overflow-hidden p-2 text-center'>
-      <ShardMain ref={ref} date={date} style={{ fontSize: fontSize + 'em' }} {...bind()} />
+      {exitingElementRef.current}
+      {primaryElementRef.current}
       <div
         className='relative col-start-1 row-start-1 flex cursor-pointer flex-col-reverse items-center justify-center font-serif text-xs [writing-mode:vertical-rl] [@media_(min-height:_640px)]:xl:text-lg'
         onClick={() => navigateDate(-1)}
