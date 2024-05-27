522652b14d49436c21f0159b5ff1757b8ef81c57
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-05-27 09:15:02 +0800

    expandable header buttons

--- src/sections/App/Header.tsx
+++ src/sections/App/Header.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { FaCog, FaCalendarDay } from 'react-icons/fa';
+import { FaCog, FaCalendarDay, FaEllipsisV, FaAngleRight } from 'react-icons/fa';
 import { DateTime } from 'luxon';
 import { DynamicCalendar } from '../../components/Calendar';
 import { ClockNow } from '../../components/Clock';
@@ -18,16 +19,46 @@ function HeaderDateTime({ navigateToday }: { navigateToday: () => void }) {
     <div
       data-nosnippet
       onClick={navigateToday}
-      className='flex cursor-pointer flex-col flex-nowrap items-center justify-center gap-x-3 text-center md:flex-row landscape:flex-row'
+      className=' flex cursor-pointer flex-col flex-nowrap items-center justify-center gap-x-3 text-center md:flex-row landscape:flex-row'
     >
+      <p className='max-md:hidden'>{t('headerDateTimeIndicator')}</p>
       <p
         className='short:swap data-[swap]:short:swap-active max-md:swap data-[swap]:max-md:swap-active tall:md:cursor-pointer tall:md:flex-col tall:md:gap-x-2'
         data-swap={dateActive}
       >
         <DynamicCalendar className='swap-on' />
-        <span className='swap-off'>{t('headerDateTimeIndicator')}</span>
+        <span className='swap-off md:hidden'>{t('headerDateTimeIndicator')}</span>
       </p>
       <ClockNow dualUnit className='text-md xs:text-2xl' relFontSize={0} />
+    </div>
+  );
+}
+
+export function HeaderButton({
+  children,
+  title,
+  isExpand = false,
+  onClick,
+}: {
+  onClick: () => void;
+  children: React.ReactNode;
+  title: string;
+  isExpand?: boolean;
+}) {
+  return (
+    <div
+      className='tooltip tooltip-bottom hidden *:transition-all data-[expand=true]:block md:block md:data-[expand=true]:hidden max-md:group-data-[expand-menu=true]:block'
+      data-tip={title}
+      data-expand={isExpand}
+    >
+      <button
+        type='button'
+        title={title}
+        className='w-min rounded-lg bg-slate-50 bg-opacity-25 p-1.5 shadow-xl shadow-zinc-700 hover:bg-opacity-50'
+        onClick={onClick}
+      >
+        {children}
+      </button>
     </div>
   );
 }
@@ -37,48 +68,51 @@ export default function Header() {
   const { setSettings } = useSettings();
   const { showModal } = useModal();
   const navigateToday = () => setSettings({ date: DateTime.local({ zone: 'America/Los_Angeles' }) });
+  const [expandMenu, setExpandMenu] = useState(false);
 
   return (
-    <header className='glass flex max-h-min flex-row flex-nowrap items-center justify-between px-5'>
-      <a href='/' onClick={e => (navigateToday(), e.preventDefault())}>
+    <header
+      className='group glass flex max-h-min flex-row flex-nowrap items-center justify-between px-4'
+      data-expand-menu={expandMenu}
+    >
+      <a
+        className='max-md:group-data-[expand-menu=true]:hidden'
+        href='/'
+        onClick={e => (navigateToday(), e.preventDefault())}
+      >
         <img src='/icons/appName.webp' alt='Sky Shards' className='h-7 w-auto md:h-10' />
       </a>
 
       <HeaderDateTime navigateToday={navigateToday} />
 
-      <div className='mr-1 flex flex-row flex-nowrap items-center justify-end gap-x-2 lg:gap-x-4'>
-        <div className='tooltip tooltip-bottom' data-tip={t('dateSelector:title')}>
-          <button
-            type='button'
-            title={t('dateSelector:title')}
-            className='w-min rounded-lg bg-slate-50 bg-opacity-25 p-1.5 shadow-xl shadow-zinc-700 hover:bg-opacity-50'
-            onClick={() => {
-              showModal({
-                children: DateSelectionModal,
-                hideOnOverlayClick: true,
-                title: t('dateSelector:title'),
-              });
-            }}
-          >
-            <FaCalendarDay size={18} />
-          </button>
-        </div>
-        <div className='tooltip tooltip-bottom' data-tip={t('settings:title')}>
-          <button
-            type='button'
-            title={t('settings:title')}
-            className='w-min rounded-lg bg-slate-50 bg-opacity-25 p-1.5 shadow-xl shadow-zinc-700 hover:bg-opacity-50'
-            onClick={() => {
-              showModal({
-                children: SettingsModal,
-                hideOnOverlayClick: true,
-                title: t('settings:title'),
-              });
-            }}
-          >
-            <FaCog size={18} />
-          </button>
-        </div>
+      <div className='flex flex-row gap-x-2'>
+        <HeaderButton
+          title={t('dateSelector:title')}
+          onClick={() => {
+            showModal({
+              children: DateSelectionModal,
+              hideOnOverlayClick: true,
+              title: t('dateSelector:title'),
+            });
+          }}
+        >
+          <FaCalendarDay size={18} />
+        </HeaderButton>
+        <HeaderButton
+          title={t('settings:title')}
+          onClick={() => {
+            showModal({
+              children: SettingsModal,
+              hideOnOverlayClick: true,
+              title: t('settings:title'),
+            });
+          }}
+        >
+          <FaCog size={18} />
+        </HeaderButton>
+        <HeaderButton isExpand title={t('dateSelector:title')} onClick={() => setExpandMenu(!expandMenu)}>
+          {expandMenu ? <FaAngleRight size={18} /> : <FaEllipsisV size={18} />}
+        </HeaderButton>
       </div>
     </header>
   );
