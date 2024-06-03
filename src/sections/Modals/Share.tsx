b83471c791eec97b3243c43dda6567e052384985
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-06-03 19:34:49 +0800

    generated info logic

--- src/sections/Modals/Share.tsx
+++ src/sections/Modals/Share.tsx
@@ -1,33 +1,66 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaCopy } from 'react-icons/fa';
 import i18next from 'i18next';
+import { DateTime } from 'luxon';
 import { ModalProps } from '../../context/ModalContext';
+import { useSettings } from '../../context/Settings';
+import { getShardInfo } from '../../data/shard';
+import { languageCode } from '../../i18n';
 
 interface GeneratedInfoSetting {
   lanugage: string;
   timezone: string | 'discord';
-  date: string;
+  date: DateTime;
   inclLink: boolean;
   inclAuthor: boolean;
 }
 
+function renderHtmlTagsAsText(text: string, subsitution?: Record<string, string>) {
+  return text.replace(/<[^>]+>/g, tag => subsitution?.[tag] || '');
+}
+
 export default function ShareModal({ hideModal }: ModalProps) {
-  const { t } = useTranslation('share');
+  const { t } = useTranslation(['share', 'settings']);
+  const { lang, timezone, date } = useSettings();
+  const [genInfoSetting, setGenInfoSetting] = useState<GeneratedInfoSetting>({
+    lanugage: lang,
+    timezone,
+    date,
+    inclLink: true,
+    inclAuthor: true,
+  });
+
   const canShare = useMemo(() => navigator.share !== undefined && navigator.canShare?.(), []);
 
-  // Separated i18next instance to only render the copying text to a different locale than the rest of the app
-  const infoLocale = useMemo(() => i18next.cloneInstance(), []);
-  const infoT = useCallback(
-    (...p: Parameters<typeof i18next.t>) => {
-      const message = infoLocale.t(...p) as string;
-      return /<[^>]+>/.test(message) ? message.replace(/<[^>]+>/g, '') : message;
-    },
-    [infoLocale],
-  ) as typeof i18next.t;
+  const generatedInfo = useMemo(() => {
+    const { lanugage, timezone, date, inclLink, inclAuthor } = genInfoSetting;
+    const { hasShard, isRed, occurrences, realm, map } = getShardInfo(date);
+    const localT = i18next.getFixedT(lanugage);
+    let msg = '';
+
+    const dateTagText = renderHtmlTagsAsText(
+      date.toLocaleString({ weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' }, { locale: lanugage }),
+    );
+    if (hasShard) {
+      const shardTagText = renderHtmlTagsAsText(localT(isRed ? 'infoSection:redShard' : 'infoSection:blackShard'));
+      const realmTagText = renderHtmlTagsAsText(localT(`skyRealms:${realm}.long`));
+      msg = renderHtmlTagsAsText(localT('infoSection:hasShard', { map }), {
+        '<br>': '\n',
+        '<shard/>': shardTagText,
+        '<realm/>': realmTagText,
+        '<date/>': dateTagText,
+      });
+    } else {
+      msg = renderHtmlTagsAsText(localT('infoSection:noShard'), {
+        '<br>': '\n',
+        '<date/>': dateTagText,
+      });
+    }
+  }, [genInfoSetting, date]);
 
   return (
-    <div className='mb-4 flex min-w-48 flex-col justify-center gap-2 md:grid md:grid-cols-2 [&>div]:w-full [&>div]:px-2'>
+    <div className='mb-4 flex min-w-48 flex-col justify-center gap-2 [&>div]:w-full [&>div]:px-2'>
       {/* Share button */}
 
       <div className='divider my-0.5'>{t('linkTitle')}</div>
@@ -60,7 +93,7 @@ export default function ShareModal({ hideModal }: ModalProps) {
       </div>
       <div className='divider my-0.5'>{t('infoTitle')}</div>
       {/* Copy/Share generated info */}
-      {infoT('')}
+      <div className='grid grid-cols-1 gap-2 md:grid-cols-2'></div>
     </div>
   );
 }
