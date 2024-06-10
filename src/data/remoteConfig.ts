800160db07c1164e8b81b52a5f8c4b66bd3391c4
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2024-06-10 16:26:03 +0800

    add swr as lightweight package to handle querying

--- src/data/remoteConfig.ts
+++ src/data/remoteConfig.ts
@@ -1,5 +1,8 @@
 // Copied from https://github.com/PlutoyDev/sky-shardfig/blob/main/shared/lib.ts
+import { useEffect } from 'react';
 import { DateTime } from 'luxon';
+import useSWR from 'swr';
+import useLegacyEffect from '../hooks/useLegacyEffect';
 import type { Translation } from '../i18n';
 
 export interface Override {
@@ -25,20 +28,41 @@ export interface DailyConfig {
 export interface RemoteConfigResponse {
   dailiesMap: Record<string, DailyConfig>;
   authorNames: Record<string, string>;
-  warnings?: 'bugged' | 'changed' | 'disabled';
+  warning?: 'bugged' | 'changed' | 'disabled';
+  warningLink?: string;
   // Randomly generated string for polling check
   id: string;
 }
 
 export type RemoteConfig = RemoteConfigResponse;
 
-export async function fetchRemoteConfig(): Promise<RemoteConfig> {
-  const res = await fetch((import.meta.env.VITE_SHARD_REMOTE_URL as string) + '/minified.json');
-  return await res.json();
-}
+// export async function fetchRemoteConfigSample(): Promise<RemoteConfig> {
+//   const res = await fetch((import.meta.env.VITE_SHARD_REMOTE_URL as string) + '/minified.json');
+//   return await res.json();
+// }
 
-export async function shouldUpdate(id: string): Promise<boolean> {
-  const res = await fetch((import.meta.env.VITE_SHARD_REMOTE_URL as string) + '/poll_id.txt');
-  const text = await res.text();
-  return text !== id;
+// export async function fetchRemoteConfigFull(): Promise<RemoteConfig> {
+//   const res = await fetch((import.meta.env.VITE_SHARD_REMOTE_URL as string) + '/all.json');
+//   return await res.json();
+// }
+
+// export async function shouldUpdate(id: string): Promise<boolean> {
+//   const res = await fetch((import.meta.env.VITE_SHARD_REMOTE_URL as string) + '/poll_id.txt');
+//   const text = await res.text();
+//   return text !== id;
+// }
+
+const fetcher = (file: 'minified.json' | 'all.json') =>
+  fetch((import.meta.env.VITE_SHARD_REMOTE_URL as string) + '/' + file).then(res => res.json()) as Promise<RemoteConfig>;
+
+export function useRemoteConfig(requireFull: boolean = false) {
+  const {data: config,} = useSWR(() => (requireFull ? 'all.json' : 'minified.json'), fetcher, {
+    refreshInterval: 20 * 60 * 1000, // 20 minutes
+    keepPreviousData: true,
+    dedupingInterval: 5 * 60 * 1000, // 5 minutes
+    focusThrottleInterval: 5 * 60 * 1000, // 5 minutes
+  });
+
+  return config;
+  
 }
